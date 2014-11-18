50668ab7389e9e4d79cc741f1c8d81c0c160a271
Andreas Platzer <andreas.platzer@gmail.com>
2014-11-18 17:43:00 +0100

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -3,34 +3,37 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix takes only a matrix as an input function
+# 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
-        set <- function(y) {
-               x <<- y
-               m <<- NULL
+        
+        set <- function(y) {    # the setter function
+               x <<- y          # the magic super assign operator
+               m <<- NULL       # just store new matrix and del old cache
        }
 
-        get <- function() x
+        get <- function() x     # the getter function
         setInverse <- function(inverse) m <<- inverse
         getInverse <- function() m
-        list(set = set, get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)	   	      	   
+        list(set = set, get = get,       # another magic thing, the list contains all
+             setInverse = setInverse, getInverse = getInverse)	# possible operations   	      	   
 }
 
+# I still dont get how on earth where and how the matrix is stored !!
+# as set is not called. is is?
 
-## Write a short comment describing this function
 
+## Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
     
-    m <- x$getInverse()
-    if(!is.null(m)) {
+    m <- x$getInverse()     # tries to read the inverse of the matrix
+    if(!is.null(m)) {       # if available - return inverse matrix
         message("getting cached matrix")
         return(m)
     }
-    data  <- x$get()
-    m  <- solve(data,...)
+    data  <- x$get()        # inverse is not available - get input matrix
+    m  <- solve(data,...)   # calculate the inverse of the input
     x$setInverse(m)
     m   # returns the inverse matrix
 }
